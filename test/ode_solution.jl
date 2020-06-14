c442379ed7043749aac13c4275576306fee2a7e3
sandreza <andrenogueirasouza@gmail.com>
2020-06-14 00:17:16 -0400

    try again

--- test/ode_solution.jl
+++ test/ode_solution.jl
@@ -29,7 +29,43 @@ y = A \ rhs
 # Exact solution
 solution = exp.( x .+ 1)
 
-@testset "ODE Test" begin
+@testset "ODE Test 1" begin
+    bool = norm(y - solution) < eps(1.0) * 100
+    @test bool
+end
+
+
+# Polynomial Order
+n = 32
+
+# Domain [a b]
+Ω = [-1.0 0.0]
+b = Ω[end]
+a = Ω[1]
+L = b - a
+
+D, cx = cheb(n)
+
+x =  (b-a) .* (cx .+ 1) ./ 2 .+ a
+
+∂ˣ = 2 / L .* D
+
+# Equation Dy = y, y[x=-1] = 1
+A = ∂ˣ - I
+
+# Boundary Conditions
+A[end, :] .= 0.0
+A[end, end] = 1.0
+
+# Solve
+rhs = 0.0 * x
+rhs[end] = 1.0
+y = A \ rhs
+
+# Exact solution
+solution = exp.( x .+ 1)
+
+@testset "ODE Test 2" begin
     bool = norm(y - solution) < eps(1.0) * 100
     @test bool
 end

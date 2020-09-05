34a207c7a9fd6aa63ee33bba710addc548a5fff3
sandreza <andrenogueirasouza@gmail.com>
2020-09-04 23:19:39 -0400

    preliminary fourier tests

--- test/fourier_tests.jl
+++ test/fourier_tests.jl
@@ -1,6 +1,25 @@
-using QuasiGeostrophy, LinearAlgebra
+using QuasiGeostrophy, LinearAlgebra, Plots, FFTW, BenchmarkTools
 
-N = 8
+N = 8^3
 x = fourier_nodes(N, a = 0, b = 2π)
 y = sin.(x)
 plot(x,y)
+tmp = y .+ 0im
+tmp2 = y .+ 0im
+
+FFTW.set_num_threads(1)
+P = plan_fft(x)
+@btime mul!(tmp, P, tmp2)
+
+##
+N = 2^8
+g  = fourier_nodes(N, a = 0, b = 2π)
+x = reshape(g, (N, 1))
+y = reshape(g, (1, N))
+f = @. sin(x) + sin(y) + 0im
+f̂ = copy(f)
+FFTW.set_num_threads(6)
+P = plan_fft(f)
+@btime mul!(f̂, P, f)
+##
+

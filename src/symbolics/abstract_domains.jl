2b9b2233c8a8bbdccd63411f41b6889243498350
sandreza <andrenogueirasouza@gmail.com>
2020-09-05 20:27:28 -0400

    adds some features to domain

--- src/symbolics/abstract_domains.jl
+++ src/symbolics/abstract_domains.jl
@@ -3,27 +3,18 @@ import LinearAlgebra: ×
 
 export AbstractDomain
 export AbstractBoundary
-export PredefinedBoundary
-export GeneralDomain
-export GeneralBoundary
 
 export DomainBoundary
-export IntervalDomain, ProductDomain, SphereSurfaceDomain
+export IntervalDomain, ProductDomain, Torus, Interval
+export ×, dim, info
 
 abstract type AbstractDomain end
 abstract type AbstractBoundary end
-abstract type PredefinedBoundary <: AbstractDomain end
-
-struct GeneralDomain <: AbstractDomain end
-∂(::GeneralDomain) = DomainBoundary(nothing)
 
 struct DomainBoundary <: AbstractBoundary
     closure
 end
-
-struct GeneralBoundary <: AbstractBoundary end
-
-struct IntervalDomain{AT, BT, PT} <: PredefinedBoundary
+struct IntervalDomain{AT, BT, PT} <: AbstractDomain
     a::AT
     b::BT
     periodic::PT
@@ -33,10 +24,57 @@ function IntervalDomain(a, b; periodic=false)
     @assert a < b
     return IntervalDomain(a, b, periodic)
 end
+function Torus(a, b)
+    @assert a < b
+    return IntervalDomain(a, b, periodic = true)
+end
+function Interval(a, b)
+    @assert a < b
+    return IntervalDomain(a, b)
+end
+
+function Base.show(io::IO, Ω::IntervalDomain) 
+    a = Ω.a
+    b = Ω.b
+    printstyled(io, "[", color = 226)
+    printstyled("$a, $b", color = 7)
+    Ω.periodic ? printstyled(io, ")", color = 226) : printstyled(io, "]", color = 226)
+ end
+
 
 ∂(Ω::IntervalDomain) = Ω.periodic ? DomainBoundary(nothing) : DomainBoundary((Ω.a, Ω.b))
 
 struct ProductDomain{DT} <: AbstractDomain
     domains::DT
 end
-×(args::AbstractDomain...) = ProductDomain{typeof(args)}(args)
+
+function Base.show(io::IO, Ω::ProductDomain) 
+    for (i,domain) in enumerate(Ω.domains)
+        print(domain)
+        if i != length(Ω.domains)
+            printstyled(io, "×", color = 118)
+        end
+    end
+ end
+
+ function dim(Ω::IntervalDomain)
+    return 1
+end
+
+function dim(Ω::ProductDomain)
+    return length(Ω.domains)
+end
+
+×(arg1::AbstractDomain, arg2::AbstractDomain) = ProductDomain((arg1, arg2))
+×(args::AbstractDomain) = ProductDomain(args...)
+
+function info(Ω::ProductDomain)
+    println("This is a ", dim(Ω),"-dimensional tensor product domain.")
+    print("The domain is ")
+    println(Ω, ".")
+    for (i,domain) in enumerate(Ω.domains)
+        domain_string = domain.periodic ? "periodic" : "wall-bounded"
+        println("The dimension $i domain is ", domain_string, " with length ", domain.b-domain.a)
+    end
+    return nothing
+end

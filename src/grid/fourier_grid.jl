fa84b3532d3f856bfd0e63a3bf7850f6f4cce2a4
sandreza <andrenogueirasouza@gmail.com>
2020-09-05 22:52:23 -0400

    slight grid adjustments

--- src/grid/fourier_grid.jl
+++ src/grid/fourier_grid.jl
@@ -1,5 +1,5 @@
 export FourierGrid
-export create_grid
+export create_grid, getindex
 
 struct FourierGrid{𝒢, 𝒲, 𝒟} <: AbstractGrid
     grid::𝒢
@@ -7,7 +7,10 @@ struct FourierGrid{𝒢, 𝒲, 𝒟} <: AbstractGrid
     domain::𝒟
 end
 
-function create_grid(grid_points, Ω::IntervalDomain)
+import Base: getindex
+getindex(f::FourierGrid, i) = f.grid[i]
+
+function create_grid(grid_points, Ω::IntervalDomain) # perhaps change to match the product domain
     @assert length(grid_points) == 1
     @assert Ω.periodic
     grid = fourier_nodes(grid_points, a = Ω.a, b = Ω.b)
@@ -15,15 +18,26 @@ function create_grid(grid_points, Ω::IntervalDomain)
     return FourierGrid(grid, wavenumbers, Ω)
 end
 
-function create_grid(grid_points, Ω::ProductDomain)
+"""
+create_grid(grid_points, Ω::ProductDomain)
+# Description
+create a numerical grid with grid_points resolution in the domain Ω
+# Arguments
+- `grid_points`: tuple | a tuple of ints in each direction for product domain
+- `Ω`: ProductDomain   | a product domain object
+# Return
+A Fourier Grid object
+"""
+function create_grid(grid_points, Ω::ProductDomain) # change to be fully general later
     @assert length(grid_points) == length(Ω.domains)
     grid = []
     wavenumbers = []
     @assert check_full_periodicity(Ω)
     for (i,domain) in enumerate(Ω.domains)
         L = domain.b - domain.a
-        push!(grid, fourier_nodes(grid_points[i], a = domain.a, b = domain.b))
-        push!(wavenumbers, fourier_wavenumbers(grid_points[i], L = L))
+        reshape_dims = appropriate_dims(length(Ω.domains), i, grid_points[i])
+        push!(grid, reshape(fourier_nodes(grid_points[i], a = domain.a, b = domain.b), reshape_dims))
+        push!(wavenumbers, reshape(fourier_wavenumbers(grid_points[i], L = L), reshape_dims))
     end
     return FourierGrid(grid, wavenumbers, Ω)
 end

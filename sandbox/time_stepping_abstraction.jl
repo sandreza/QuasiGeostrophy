17fd4d88fa910970d17c89230d7d651c4b8914c7
sandreza <andrenogueirasouza@gmail.com>
2020-09-07 10:27:05 -0400

    adds pde_system example

--- sandbox/time_stepping_abstraction.jl
+++ sandbox/time_stepping_abstraction.jl
@@ -39,10 +39,11 @@ function Base.show(io::IO, field::Wrapper{T, S}) where {T, S <: MetaData}
 end
 
 u = Wrapper("u", MetaData("u"))
+σ = Wrapper("σ", MetaData("σ"))
 ν = Wrapper(0.001, MetaData("ν"))
 ∂x(u) = Gradient(u, GradientMetaData("x"))
 ∂t(u) = Gradient(u, GradientMetaData("t"))
-∂x(u)
+tmp = ∂x(u)
 ##
 function Base.show(io::IO, ∇::Gradient) 
     color = 220
@@ -110,12 +111,16 @@ function get_implicit(a::Implicit)
     return a
 end
 function get_implicit(a::Explicit)
-    return 0 # probably need a rule like 0 + field = field
+    return Field(0) # probably need a rule like 0 + field = field
 end
 function get_implicit(a::Add)
     t1 = get_implicit(a.term1)
     t2 = get_implicit(a.term2)
     return t1 + t2
+end
+function get_implicit(a::Negative)
+    t1 = get_implicit(a.term)
+    return t1
 end
 
 function get_explicit(a::Explicit)
@@ -123,7 +128,7 @@ function get_explicit(a::Explicit)
 end
 
 function get_explicit(a::Implicit)
-    return 0 # probably need a rule like 0 + field = field
+    return Field(0) # probably need a rule like 0 + field = field
 end
 
 function get_explicit(a::Add) 
@@ -131,16 +136,41 @@ function get_explicit(a::Add)
     t2 = get_explicit(a.term2)
     return t1 + t2
 end
+function get_explicit(a::Negative)
+    t1 = get_explicit(a.term)
+    return t1
+end
 
 function get_implicit(eq::AbstractEquation)
     # place all implicit terms on left-hand side
     implicit = get_implicit(eq.lhs) - get_implicit(eq.rhs)
-    # place all explicit terms on right-hand side
-    explicit = -get_explicit(eq.lhs) + get_implicit(eq.rhs)
+    return implicit
+end
+function get_explicit(eq::AbstractEquation)
+    # place all explicit terms on left-hand side
+    explicit = get_explicit(eq.rhs) - get_explicit(eq.lhs)
+    return explicit
+end
+function get_imex(eq::AbstractEquation)
+    implicit = get_implicit(eq)
+    explicit = get_explicit(eq)
     return implicit == explicit
+end
+function get_imex(system::Array{S}) where {S <: AbstractEquation}
+    return [get_imex(equation) for equation in system]
 end
 ##
 tmp_expr = Explicit(-∂x(u*u), AB1()) + Implicit(ν * ∂x(∂x(u)) , BDF1()) + Explicit(-∂x(u*u), AB1()) + Explicit(-∂x(u*u), AB1()) 
 tmp_implicit = get_implicit(tmp_expr)
 tmp_implicit = get_explicit(tmp_expr)
-new_eq  = get_implicit(Implicit(∂t(u), BDF1()) == Explicit(-∂x(u*u), AB1()) + Implicit(ν * ∂x(∂x(u)) , BDF1()))
+new_eq  = get_imex(Implicit(∂t(u), BDF1()) == Explicit(-∂x(u*u), AB1()) + Implicit(ν * ∂x(∂x(u)) , BDF1()))
+##
+pde_system = [
+    Implicit(σ, BDF1()) == Implicit(∂x(u), BDF1()),
+    Implicit(∂t(u), BDF1()) == Explicit(- ∂x( u * u), AB1())+ Implicit(ν *  ∂x(σ), BDF1())
+]
+pde_system[1]
+pde_system[2]
+new_system = get_imex(pde_system)
+eq1 = pde_system[1]
+eq2 = pde_system[2]

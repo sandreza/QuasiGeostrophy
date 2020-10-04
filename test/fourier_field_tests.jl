5e596844b3e5986dbd0d407c6553f37951bf965c
sandreza <andrenogueirasouza@gmail.com>
2020-10-04 12:28:22 -0400

    minor cosmetic changes to test

--- test/fourier_field_tests.jl
+++ test/fourier_field_tests.jl
@@ -4,13 +4,13 @@ include(pwd() * "/test/test_utils.jl")
 const tol = 1e1
 
 ## Define 1D Test
-Ωxy = Torus(0,2π) 
+Ω = Torus(0,2π) 
 Nx = 2^4; 
-fourier_grid = create_grid(Nx, Ωxy)
+fourier_grid = create_grid(Nx, Ω)
 x = fourier_grid.grid[1]
 fourier_transform = Transform(fourier_grid)
 
-names = ("ϕ1", "ϕ2", "ϕ3", "ϕ4")
+names = ("ϕ1", "ϕ2", "ϕ3", "ϕ4", "ϕ5")
 
 create_fields(names = names, 
               grid = fourier_grid,
@@ -19,16 +19,28 @@ create_fields(names = names,
 ϕ1(sin.(x)); ϕ2(cos.(x));
 ϕ3( @. sin(x) * cos(x) );
 ϕ4( @. sin(x) + cos(x) );
+ϕ5( @. cos(x)^2 - sin(x)^2 );
 
 @testset "1D Field Algebra Test" begin
+    println("checking ϕ1 * ϕ2 - ϕ3")
     @test norm(ϕ1 * ϕ2 - ϕ3)/norm(ϕ3) < eps(tol)
+    println("checking ϕ1 + ϕ2 - ϕ4")
     @test norm(ϕ1 + ϕ2 - ϕ4)/norm(ϕ4) < eps(tol)
+end
+println(" ")
+create_operators(fourier_grid)
+
+@testset "1D Field Calculus Test" begin
+    println("checking ∂x(ϕ1)  - ϕ2")
+    @test norm(∂x(ϕ1)  - ϕ2)/norm(ϕ2) < eps(tol)
+    println("checking ∂x(ϕ3)  - ϕ5")
+    @test norm(∂x(ϕ3)  - ϕ5)/norm(ϕ5) < eps(tol)
 end
 
 ## Define 2D Test
-Ωxy = Torus(0,2π) × Torus(0,2π)
+Ω = Torus(0,2π) × Torus(0,2π)
 Nx = 2^4; Ny = 2^4;
-fourier_grid = create_grid((Nx, Ny), Ωxy)
+fourier_grid = create_grid((Nx, Ny), Ω)
 x, y = fourier_grid.grid
 fourier_transform = Transform(fourier_grid)
 

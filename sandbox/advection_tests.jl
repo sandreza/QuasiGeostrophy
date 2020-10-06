b1dd0bfb530b4369b2df6b4d363a6981fe67272e
sandreza <andrenogueirasouza@gmail.com>
2020-10-05 22:59:36 -0400

    bugfix

--- sandbox/advection_tests.jl
+++ sandbox/advection_tests.jl
@@ -19,7 +19,6 @@ iv = @. tanh( ν*(x - π/2)) * tanh(ν*(x - 3π/2))
 ϕ2(iv)
 plot(ϕ)
 Δt = 0.01 / scale * 2π
-push!(Δt_list_2, Δt)
 t = randn(1) .* 0
 for i in 1:100*scale
     Δ = ∂x^2
@@ -57,7 +56,6 @@ iv = @. tanh( ν*(x - π/2)) * tanh(ν*(x - 3π/2) ) * tanh( ν*(y - π/2)) * ta
 ϕ2(iv)
 plot(ϕ)
 Δt = 0.01 / scale * 2π
-push!(Δt_list_2, Δt)
 t = randn(1) .* 0
 #u( (x .+ y) .* 0 .+1 )
 #v( (x .+ y) .* 0 .+1 )

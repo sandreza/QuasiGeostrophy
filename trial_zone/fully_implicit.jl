4a513ef97b89a1c9426d779e54d5b3c99c75c082
sandreza <andrenogueirasouza@gmail.com>
2020-12-28 14:46:14 -0500

    bugfix, add fixed point in the future

--- trial_zone/fully_implicit.jl
+++ trial_zone/fully_implicit.jl
@@ -8,7 +8,7 @@ const tol = 1e1
 gr(size = (300,300))
 # Define 1D Test
 Ω = S¹(0, 2π) 
-Nx = Ny = Nz = 2^3; 
+Nx = Ny = Nz = 2^6; 
 fourier_grid = FourierGrid(Nx, Ω)
 fieldnames = ("u", "σ", "v", "δu")
 u, σ, v, δu = create_fields(names = fieldnames, grid = fourier_grid)
@@ -67,8 +67,8 @@ end
 u(sin.(x) .+ 0.5); σ(cos.(x));
 v(sin.(x));
 
-const Δt = 1.0 / Nx  
-const ν = 0.0/(2π)^2 * Δx^2 / Δt
+const Δt = 0.1 / Nx  
+const ν = 0.01/(2π)^2 * Δx^2 / Δt
 
 # Backward-Euler
 # uⁿ⁺¹ = uⁿ + Δt u̇ⁿ⁺¹
@@ -79,8 +79,8 @@ plotsteps = round(Int, steps / 10)
 for i in 1:steps
     v.data .= u.data
     # initial guess for δu
-    #δu.data .= Δt * (- ∂x( 0.5 * u * u) + ν * Δ(u)).data
-    δu.data .= 0.0
+    δu.data .= Δt * (- ∂x( 0.5 * u * u) + ν * Δ(u)).data
+    #δu.data .= 0.0
     u.data .+= δu.data
     # newton iteration
     newtonN = 1
@@ -97,9 +97,8 @@ for i in 1:steps
 end
 
 ## Semiplicit
-# solve uⁿ⁺¹ - Δt *( ∂x( 0.5 * uⁿ * uⁿ⁺¹) - νΔuⁿ⁺¹ ) = uⁿ
+# solve uⁿ⁺¹ - Δt *( -∂x( 0.5 * uⁿ * uⁿ⁺¹) + νΔuⁿ⁺¹ ) = uⁿ
 # Semi-Implicit
-
 const Δt = 0.1 / Nx  
 const ν = 0.0/(2π)^2 * Δx^2 / Δt
 
@@ -108,15 +107,48 @@ steps = round(Int, T / Δt)
 plotsteps = round(Int, steps / 10)
 
 function semiplicit(u, δu, ν, Δt)
-    return δu - Δt *( 0.5*∂x(u * δu) - ν*Δ(δu) )
+    v = u + Δt * (- ∂x( 0.5 * u * u) + ν * Δ(u))
+    return δu + Δt *( 0.5*∂x(v * δu) - ν*Δ(δu) )
 end
 
 u(sin.(x) .+ 0.5); σ(cos.(x));
 v(sin.(x));
 
 for i in 1:steps
-    S(δu) = semiplicit(u, δu, ν, Δt)
-    A = create_matrix(S, δu)
     v.data .= u.data
+    Snew(δu) = semiplicit(u, δu, ν, Δt)
+    A = create_matrix(Snew, δu)
     u.data .= A \ v.data
+    println(i)
+end
+
+
+## Backward-Euler with semiplicit guess
+# uⁿ⁺¹ = uⁿ + Δt u̇ⁿ⁺¹
+u(sin.(x) .+ 0.5); σ(cos.(x));
+v(sin.(x));
+T = 1.5
+const Δt = 0.1 / Nx  
+const ν = 0.0/(2π)^2 * Δx^2 / Δt
+steps = round(Int, T / Δt) 
+plotsteps = round(Int, steps / 10)
+
+for i in 1:steps
+    v.data .= u.data
+    # initial guess for δu
+    Snew(δu) = semiplicit(u, δu, ν, Δt)
+    A = create_matrix(Snew, δu)
+    u.data .=  A \ v.data 
+    # newton iteration
+    newtonN = 1
+    for j in 1:newtonN
+        residual = Residual(u, v, ν, Δt)
+        #println("residual at ", i, " is ", norm(residual))
+        J(δu) = Jacobian(u.data, δu, ν, Δt)
+        A = create_matrix(J, δu)
+        δu.data .= A \ residual.data
+        u.data .+= δu.data
+    end
+    residual = Residual(u, v, ν, Δt)
+    println("residual at ", i, " now is ", norm(residual))
 end
